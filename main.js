f6308b5b0e2c6dcd0a80f9816a6c6359f9501416
mariadenise123 <denise.oliveira.maria@escola.pr.gov.br>
2026-04-23 15:00:42 -0300

    iiuu

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const botoes =document.querySelectorAll(botoes)
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes [i] .onclick = function (){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes [i] this.classList.add("ativo")
+ 
+        }
+        
+        
+    }
+    console.log()
+}

8ee6bbe9fdc8afd9fd16ab9f2b8d3e771e6e2e6a
mariadenise123 <denise.oliveira.maria@escola.pr.gov.br>
2026-04-24 09:30:38 -0300

    gggbg

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ for(let i=0;i <botoes.length;i++){
     botoes [i] .onclick = function (){
 
         for(let j=0;j<botoes.length;j++){
-            botoes [i] this.classList.add("ativo")
- 
+            botoes[i].classList.remove("ativo");
         }
         
-        
+        botoes[i].classList.add("ativo");
     }
     console.log()
 }
